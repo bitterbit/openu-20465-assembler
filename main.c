894e3065c60f054bfbc3fed4d4555afd517e722a
AlonMenczer <alonm@spotnix.io>
2021-08-14 00:15:04 +0300

    Fix number to memory bugs

--- main.c
+++ main.c
@@ -18,9 +18,7 @@ void printError(ErrorType err, AssemblyLine *line) {
 }
 /* TODO: Check for memory leaks */
 /* TODO: Compile and check on the openu ubuntu */
-/* TODO: Verify all syscalls (memory, etc..) are done safely, checked for errors */
 /* TODO: Verify the semantics of entry and external lines? are there any others? */ 
-/* TODO: implement output table for external and entry symbols */
 
 
 ErrorType saveOutput(char* name, Memory *memory, SymbolManager *syms) {
