1a6c5dfe504cc1be235c05747b85162632b79ea8
AlonMenczer <alonm@spotnix.io>
2021-08-14 00:16:06 +0300

    Remove unneeeded

--- bit_utils.c
+++ bit_utils.c
@@ -10,18 +10,3 @@ bool number_fits_in_bits(int number, int number_of_bits) {
     return (abs(number) <= pow(2, number_of_bits));
 }
 
-
-/* TODO: uneeded? */
-/* TODO: doesn't handle negative numbers  - https://stackoverflow.com/questions/1024389/print-an-int-in-binary-representation-using-c*/
-void write_binary_stream_to_buffer(int num, int number_of_bits,
-                                   unsigned char *buffer) {
-    int i = number_of_bits;
-    buffer += i;
-    printf("write_binary_stream_to_buffer\n");
-    do {
-        printf("%d", num % 2);
-        buffer[i--] = num % 2;
-        num /= 2;
-    } while (i != 0);
-    printf("\n");
-}
